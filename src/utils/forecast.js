6ca6e9532ba47771eb613df2757808b29c9aa52e
RajarshiRay97 <rajarshiray2016@gmail.com>
2024-08-21 14:48:51 +0530

    Changed weather data from weatherstack API

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 // // Our own asynchronous function for getting the weather forcast (input: latitude and longitude, output: weather status) activity
 const forecast = (latitude, longitude, callback)=>{
-    const url = `http://api.weatherstack.com/current?access_key=a3fa8fcd0331852d09361485a901185d&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}&units=f`;
+    const url = `http://api.weatherstack.com/current?access_key=a3fa8fcd0331852d09361485a901185d&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`;
 
     request({
         url,
@@ -17,7 +17,7 @@ const forecast = (latitude, longitude, callback)=>{
         else {
             const data = body.current;
 
-            callback(undefined, `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees Fahrenheit outside. It feels like ${data.feelslike} degrees Fahrenheit out.`);
+            callback(undefined, `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees Celsius outside. It feels like ${data.feelslike} degrees Celsius out. The wind speed is ${data.wind_speed} km/hr and the humidity is ${data.humidity}% currently.`);
         }
     });
 };
